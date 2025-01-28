e14476f2a86a16eacbbd0803b7eef52cf5106de5
ShahidulMamun <mdshahidulcse@gmail.com>
2025-01-28 19:01:39 +0600

    Java Variavle

---
+++ variable.java
@@ -0,0 +1,22 @@
+public class variable {
+    public static void main(String[] args) {
+        // String - stores text, such as "Hello". String values are surrounded by double quotes
+        // int - stores integers (whole numbers), without decimals, such as 123 or -123
+        // float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+        // char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+        // boolean - stores values with two states: true or false 
+        String name = "Shahidul Islam Mamun";
+        int age = 28;
+        double cgpa = 3.32;
+        boolean status = false;
+        char grade = 'A';
+
+        System.out.println("Name  :"+ name);
+        System.out.println("Age   :" + age);
+        System.out.println("CGPA  :" + cgpa);
+        System.out.println("Status:" + status);
+        System.out.println("Grade :" +grade);
+        
+
+    }
+}
