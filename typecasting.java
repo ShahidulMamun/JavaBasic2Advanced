0b0b7249d1be478752e2038eb69b35454cbd5300
ShahidulMamun <mdshahidulcse@gmail.com>
2025-01-28 23:50:15 +0600

    type casting

---
+++ typecasting.java
@@ -0,0 +1,18 @@
+public class typecasting {
+    public static void main(String[] args) {
+
+        // Widening Casting (automatically) - converting a smaller type to a larger type size
+        // byte -> short -> char -> int -> long -> float -> double
+
+        //  Narrowing Casting (manually) - converting a larger type to a smaller size type
+        //  double -> float -> long -> int -> char -> short -> byte
+
+        int mynum =10;
+        double mydouble = mynum;
+         
+        System.out.println(mynum);
+        System.out.println(mydouble);
+
+
+    }
+}
