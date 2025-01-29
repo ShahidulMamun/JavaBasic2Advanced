080c0c6b33ac8a2c80f3942ed15fcd5ffb5ea240
ShahidulMamun <mdshahidulcse@gmail.com>
2025-01-30 00:21:59 +0600

    string method

---
+++ string/findingcharracter.java
@@ -0,0 +1,8 @@
+package string;
+
+public class findingcharracter {
+    public static void main(String[] args) {
+        String txt = "Hello coder! Good Morning";
+           System.out.println(txt.indexOf("Good"));
+    }
+}
---
+++ string/stringlenth.java
@@ -0,0 +1,12 @@
+package string;
+
+public class stringlenth {
+    public static void main(String[] args) {
+        String message = "Hello coder! Good Morning";
+        System.out.println("Message :" + message);
+        System.err.println("Meaage Length :" + message.length());
+        System.out.println(message.toUpperCase());
+        System.err.println(message.toLowerCase());
+    }
+    
+}
