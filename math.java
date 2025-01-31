a312c3e0049be61c14857b83986b2b669957cb4a
ShahidulMamun <mdshahidulcse@gmail.com>
2025-01-31 22:21:50 +0600

    math methods

---
+++ math.java
@@ -0,0 +1,16 @@
+public class math {
+    public static void main(String[] args) {
+        // find the max valu from 2 
+        System.out.println(Math.max(10, 20));
+        //find the minimum valu from 2
+        System.out.println(Math.min(10,20));
+        //find square root 
+        System.out.println(Math.sqrt(9));
+
+         //for absulate value
+         System.out.println(Math.abs(-5));
+         //for generate random number
+         System.out.println(Math.random());  
+
+    }
+}
